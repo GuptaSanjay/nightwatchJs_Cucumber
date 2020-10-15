a5b5a618434233eebee61eb8bb8c34b912fdd427
GuptaSanjay <37616556+GuptaSanjay@users.noreply.github.com>
2020-10-15 14:32:15 +0530

    Update nightwatch.conf.js
    removed local config

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,51 +5,6 @@
     disable_colors: true,
     silent: !process.env.NIGHTWATCH_VERBOSE,
     test_settings: {
-      default: {
-        selenium: {
-          start_process: true,
-          server_path: seleniumServer.path,
-          port: 5555,
-          cli_args: {
-            'webdriver.chrome.driver': chromedriver.path,
-          },
-        },
-        screenshots: {
-          enabled: true,
-          path: 'screenshots',
-        },
-        page_objects_path: 'page-objects',
-        use_xpath: true,
-        desiredCapabilities: {
-          browserName: 'chrome',
-          acceptSslCerts: true,
-          acceptInsecureCerts: true,
-          chromeOptions: {
-            w3c: false,
-            args: ['disable-web-security', 'allow-running-insecure-content', '--window-size=1920,1080'],
-            perfLoggingPrefs: {
-              enableNetwork: true,
-            },
-          },
-          'goog:loggingPrefs': { driver: 'ALL', server: 'ALL', browser: 'ALL', performance: 'ALL' },
-        },
-        launch_url: 'https://illustration.test.globalbankers.net/',
-        globals: {
-          waitForConditionTimeout: 45000,
-          request_timeout_options: { timeout: 100000, retry_attempts: 5 },
-        },
-      },
-      test_environment: {
-        extends: 'default',
-        webdriver: {
-          start_process: true,
-          server_path: chromedriver.path,
-          host: '127.0.0.1',
-          port: 5555,
-          cli_args: ['--port=5555'],
-        },
-        launch_url: process.env.REACT_APP_LAUNCH_URL,
-      },
       chrome_cloud: {
         selenium: {
           start_process: false,
